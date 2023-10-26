758d6986a47c4062cedd1763d0c5e9ff09cbf33a
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-26 16:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 function firstChar(text) {
   // your code here
+
+	if(text === " ")
+	{
+		return text;
+	}
+	else if(text !== " ")
+	{
+		for(i=0; i<text.length; i++)
+			{
+				let currChar = text[i]; 
+				if(currChar === " ")
+				{
+					continue;
+			}
+	
+		else if(currChar !== " ")
+	{ 
+		return currChar;
+	}
+}
+	}
 }
 
+  
+	   
 // Do not change the code below
 
 const text = prompt("Enter text:");
-alert(firstChar(text));
+alert(firstChar(text)); 
